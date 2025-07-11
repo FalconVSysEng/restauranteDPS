30400569021e46fbc14c67788d56d97780c4be5c
veronika <veronikafalcon2004@gmail.com>
2025-07-11 16:58:54 -0500

    creando apps, modelos y urls

---
+++ restaurante/restaurante/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+import usuarios, reservas, gestion
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', views.logear, name='logear'),
+    path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
+    path('usuarios/', include(usuarios.urls)),
+    path('reservas/', include(reservas.urls)),
+    path('gestion/', include(gestion.urls))
+]
---
+++ restaurante/restaurante/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.messages import get_messages
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+def logear(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            storage = get_messages(request)
+            list(storage)
+            return redirect('home')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    return render(request, 'login.html', {})
+
+
+def cerrar_sesion(request):
+    logout(request)
+    storage = get_messages(request)
+    list(storage)
+    messages.success(request, 'Has cerrado sesión correctamente')
+    return redirect('logear')
+
+
