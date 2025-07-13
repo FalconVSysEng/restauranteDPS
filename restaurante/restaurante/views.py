34b9b4cab77b07b862bca0e12f1ed0de75b12199
veronika <veronikafalcon2004@gmail.com>
2025-07-13 01:40:47 -0500

    corrigiendo errores en creacion de usuario

--- restaurante/restaurante/views.py
+++ restaurante/restaurante/views.py
@@ -6,17 +6,20 @@ from django.contrib import messages
 
 def logear(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '').strip()
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
-            login(request, user)
-            storage = get_messages(request)
-            list(storage)
-            return redirect('home')
+            if user.is_active:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Tu cuenta está inactiva. Contacta al administrador.')
         else:
-            messages.error(request, 'Usuario o contraseña incorrectos')
-    return render(request, 'login.html', {})
+            messages.error(request, 'Usuario o contraseña incorrectos.')
+
+    return render(request, 'login.html')
 
 
 def cerrar_sesion(request):
--- restaurante/usuarios/admin.py
+++ restaurante/usuarios/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Usuario
 
-# Register your models here.
+
+class UsuarioCreationForm(UserCreationForm):
+    class Meta:
+        model = Usuario
+        fields = ('username', 'dni', 'first_name', 'last_name', 'email')
+
+
+class UsuarioChangeForm(UserChangeForm):
+    class Meta:
+        model = Usuario
+        fields = ('username', 'dni', 'first_name', 'last_name', 'email')
+
+
+class UsuarioAdmin(BaseUserAdmin):
+    add_form = UsuarioCreationForm
+    form = UsuarioChangeForm
+    model = Usuario
+
+    list_display = ('username', 'dni', 'first_name', 'last_name', 'email', 'is_active', 'is_staff')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Información personal', {'fields': ('dni', 'first_name', 'last_name', 'email')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'dni', 'first_name', 'last_name', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('username', 'dni', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+
+
+admin.site.register(Usuario, UsuarioAdmin)
